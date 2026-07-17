c4e68843da066ee22224c62eadfe4353bd479e9f
JanineChrystal <chrystalampusta@gmail.com>
2026-07-17 08:14:05 +0800

    fix: get-all-categories action.

--- src/lib/db/queries/post/get-all-categories.ts
+++ src/lib/db/queries/post/get-all-categories.ts
@@ -1,6 +1,6 @@
+import { CATEGORY_METADATA } from "@/app/(public)/constants/category-meta";
 import { db } from "@/lib/db";
 import type { Category } from "@/lib/types/post";
-import { CATEGORY_METADATA } from "../../../app/(public)/constants/category-meta";
 
 export async function getAllCategories(): Promise<Category[]> {
 	const allCategories = await db.query.categories.findMany();
