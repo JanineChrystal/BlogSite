234eebdc57e7a9870e7fad915fb8851322fcceba
JanineChrystal <chrystalampusta@gmail.com>
2026-07-17 09:58:46 +0800

    fixed: Delete revalidation path.

--- src/lib/actions/post-management/delete-post.ts
+++ src/lib/actions/post-management/delete-post.ts
@@ -13,8 +13,11 @@ export async function deletePostAction(postId: string) {
 		// Deletes the post matching the provided ID from the database
 		await db.delete(posts).where(eq(posts.id, postId));
 
-		// Tells Next.js to clear the cache and refetch the table data
-		revalidatePath("/admin/posts");
+		// Refresh the UI to show the updated post on the Admin Interface
+		revalidatePath("/admin/post-management");
+
+		// Refresh the UI to show the updated post on the public site
+		revalidatePath("/");
 
 		return { success: true };
 	} catch (err) {
